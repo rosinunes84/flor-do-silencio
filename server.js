ecdaa56b91e2b688aad2684c418dd719c84ba4a1
rosinunes84 <rosiane.nunes84@gmail.com>
2025-08-24 17:37:05 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
-import checkoutRouter from "./routes/checkoutRouter.js"; // <-- nome correto
-import freteRouter from "./routes/freteRouter.js";
+import checkoutRouter from "./routes/checkoutRouter.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
+// Verifica token
+if (!process.env.MERCADO_PAGO_ACCESS_TOKEN) {
+  console.error("❌ ERRO: MERCADO_PAGO_ACCESS_TOKEN não encontrado no .env");
+  process.exit(1);
+}
+
 // Rotas
 app.use("/checkout", checkoutRouter);
-app.use("/frete", freteRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
